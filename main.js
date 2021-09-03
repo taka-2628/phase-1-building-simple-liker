0e3ffd8e4ad581f5fc15e2b881aa7e0c291eaba2
Takashi <th2826@columbia.edu>
2021-09-03 16:54:01 -0400

    Completed assignment

--- main.js
+++ main.js
@@ -2,9 +2,45 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// grab modal
+const modal = document.querySelector('#modal')
+// grab hearts (collection of hearts)
+const hearts = document.querySelectorAll('.like-glyph')
 
+function liker (){
+  for (const heart of hearts){
+    // add event listener to each heart in hearts
+    heart.addEventListener('click', () => {
+      // make a server call
+      mimicServerCall()
+      // when successful (resolve), change the heart
+      //    if the heart if empty, make it full
+      //    if the heart if full, make it empty 
+      .then(() => {
+        if (heart.textContent == EMPTY_HEART){
+          heart.textContent = FULL_HEART;
+          heart.className = 'activated-heart';
+        } else {
+          heart.textContent = EMPTY_HEART;
+          heart.className = 'like-glyph';
+        }
+      })
+      // when failed (reject), make the error message visible 
+      .catch(error => {
+        modal.hidden = false;
+        const modalMessage = document.querySelector('#modal-message');
+        modalMessage.innerText = error;
+        // make the error message disappear (hidden) after 3 seconds
+        setTimeout(() => {
+          modal.hidden = true;
+        }, 3000);
+      })
+    }
+  )}
+}
 
+// Initialize liker function when DOM is loaded
+document.addEventListener('DOMContentLoaded', liker);
 
 
 //------------------------------------------------------------------------------
