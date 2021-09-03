622bd86e81bebdacd2b9ff5ca10ca4e1b5e0ec36
Takashi <th2826@columbia.edu>
2021-09-03 17:49:06 -0400

    c

--- main.js
+++ main.js
@@ -2,47 +2,6 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// grab modal
-const modal = document.querySelector('#modal')
-
-// grab hearts (collection of hearts)
-const hearts = document.querySelectorAll('.like-glyph')
-
-function liker (){
-  for (const heart of hearts){
-    // add event listener to each heart in hearts
-    heart.addEventListener('click', () => {
-      // make a server call
-      mimicServerCall()
-      // when successful (resolve), change the heart
-      //    if the heart if empty, make it full
-      //    if the heart if full, make it empty 
-      .then(() => {
-        if (heart.textContent == EMPTY_HEART){
-          heart.textContent = FULL_HEART;
-          heart.className = 'activated-heart';
-        } else {
-          heart.textContent = EMPTY_HEART;
-          heart.className = 'like-glyph';
-        }
-      })
-      // when failed (reject), make the error message visible 
-      .catch(error => {
-        modal.hidden = false;
-        const modalMessage = document.querySelector('#modal-message');
-        modalMessage.innerText = error;
-        // make the error message disappear (hidden) after 3 seconds
-        setTimeout(() => {
-          modal.hidden = true;
-        }, 3000);
-      })
-    }
-  )}
-}
-
-// Initialize liker function when DOM is loaded
-document.addEventListener('DOMContentLoaded', liker);
-
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
@@ -60,3 +19,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
